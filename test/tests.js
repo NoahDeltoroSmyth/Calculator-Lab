4ea6e3969fcec8703b6f10d8ea2a7496c728d232
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2021-08-26 15:48:33 -0700

    fixed/added missing tests

--- test/tests.js
+++ test/tests.js
@@ -1,24 +1,36 @@
-// IMPORT MODULES under test here:
+
 import { add } from '../calculator.js';
-// import { add } from '../calculator.js';
+
+import { subtract } from '../calculator.js';
+
+import { multiply } from '../calculator.js';
+
+import { divide } from '../calculator.js';
+
 
 const test = QUnit.test;
 
-// name your test by what it is testing
-test('add function', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
-    const number1 = 4;
-    const number2 = 6;
 
+test ('add function', (expect) => {
     const expected = 10;
-    //Act 
-    const result = add (number1, number2)
-    
-    // Call the function you're testing and set the result to a const
-    const actual = result; // use your function here
+    const actual = add(5, 5);
+    expect.equal(actual, expected);
+});
 
-    //Expect
-    // Make assertions about what is expected versus the actual result
+test ('subtract function', (expect) => {
+    const expected = 1;
+    const actual = subtract(4, 3)
+    expect.equal(actual, expected);
+});
+
+test ('multiply function', (expect) => {
+    const expected = 10;
+    const actual = multiply(5, 2);
+    expect.equal(actual, expected);
+});
+
+test ('divide function', (expect) => {
+    const expected = 10;
+    const actual = divide(20, 2);
     expect.equal(actual, expected);
 });
