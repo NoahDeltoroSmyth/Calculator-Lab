7b75ff408025bdd87c70e86ae359b7690c73b94e
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2021-08-26 11:31:39 -0700

    Created html and styles

--- app.js
+++ app.js
@@ -3,7 +3,12 @@
 // reference needed DOM elements
 
 
+
 // set event listeners 
+
     // get user input(s)
+    const num1 = Number(addNumber1.value);
+
     // do any needed work with the value(s)
+    
     // update DOM to reflect new value(s)
